2b4e8382e24997e92227a6bfd84320a83e4b88e6
barthflo <flrnt.barth@gmail.com>
2021-02-03 14:24:47 +0100

    added social media route

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,11 +4,13 @@ const projectsRouter = require('./projects.routes.js');
 const categoriesRouter = require('./categories.routes.js');
 const picturesRouter = require('./pictures.routes.js');
 const aboutRouter = require('./about.routes.js');
+const socialMediaRouter = require('./social_media.routes.js');
 
 router.use('/admins', adminRouter);
 router.use('/projects', projectsRouter);
 router.use('/categories', categoriesRouter);
 router.use('/pictures', picturesRouter);
 router.use('/about', aboutRouter);
+router.use('/social-media', socialMediaRouter);
 
 module.exports = router;
