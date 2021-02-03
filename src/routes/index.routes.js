b2952f75f3e5a9b8a8a10b3fe9874244c607fbcc
barthflo <flrnt.barth@gmail.com>
2021-02-03 14:06:14 +0100

    made routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
 const adminRouter = require('./admin.routes.js');
+const projectsRouter = require('./projects.routes.js');
+const categoriesRouter = require('./categories.routes.js');
+const picturesRouter = require('./pictures.routes.js');
+const aboutRouter = require('./about.routes.js');
 
 router.use('/admins', adminRouter);
+router.use('/projects', projectsRouter);
+router.use('/categories', categoriesRouter);
+router.use('/pictures', picturesRouter);
+router.use('/about', aboutRouter);
 
 module.exports = router;
