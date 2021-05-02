dd6fb5f783752faf590ba755a5ea6cf0363ef88a
barthflo <flrnt.barth@gmail.com>
2021-05-02 14:20:46 +0200

    project create route

--- controlers/project.controler.js
+++ controlers/project.controler.js
@@ -28,6 +28,65 @@ exports.findAll = async (req, res) => {
 	}
 };
 
+exports.findAllCategories = async (req, res) => {
+	try {
+		const categories = await db.Category.findAll({
+			order: [['name', 'ASC']],
+		});
+		if (categories === null) {
+			res.status(404).json('No results');
+		} else {
+			res.json(categories);
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err.toString());
+	}
+};
+
+exports.create = async (req, res) => {
+	try {
+		console.log(req.body);
+		const newProject = await db.Project.create({
+			title: req.body.title,
+			description: req.body.description,
+			link_url: req.body.url,
+		});
+		newProject.addCategory(req.body.results);
+		console.log(newProject);
+		res.json({ success: `Project ${newProject.id} created!`, newProject });
+	} catch (err) {
+		console.error(err);
+		res.status(500).json(err.toString());
+	}
+};
+exports.createCategory = async (req, res) => {
+	try {
+		const newCategory = await db.Category.create(req.body);
+		console.log(newCategory);
+		res.json({ success: `Category ${req.body.name} added`, newCategory });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err.toString());
+	}
+};
+
+exports.update = async (req, res) => {
+	const id = parseInt(req.params.id);
+	try {
+		await db.Project.update(req.body, {
+			where: { id },
+		});
+		res.json({
+			success: `Project ${id} successfully updated`,
+			update: req.body,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err.toString());
+	}
+};
+
 exports.delete = async (req, res) => {
 	const id = parseInt(req.params.id);
 	try {
--- routes/project.routes.js
+++ routes/project.routes.js
@@ -4,6 +4,14 @@ const { verifyJWT } = require('../services/verifyJWT');
 
 router.get('/', project.findAll);
 
+router.get('/categories', project.findAllCategories);
+
+router.post('/', verifyJWT, project.create);
+
+router.post('/categories', verifyJWT, project.createCategory);
+
+router.put('/:id', verifyJWT, project.update);
+
 router.delete('/:id', verifyJWT, project.delete);
 
 module.exports = router;
